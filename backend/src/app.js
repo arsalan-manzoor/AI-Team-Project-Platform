fe573fdfcb4a31608831ac935a5a2a9c0211c718
Muneeb Rashid <muneebrashid2005@gmail.com>
2026-09-17 16:09:00 +0530

    Restructure backend server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
+
 const app = express();
-const PORT = 5000;
+
 app.get("/", (req, res) => {
     res.send("Backend is working");
 });
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
+module.exports = app;
